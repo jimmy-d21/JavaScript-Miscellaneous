4d0dcc6d8f81e6d8e33cbb5eaa7fd34433db8856
jimmyedelacruz <jimmyedelacruz872@gmail.com>
2026-09-15 23:20:04 +0800

    Checking primitive types

---
+++ JavaScript-Miscellaneous.js
@@ -0,0 +1,7 @@
+// 1. Type Checking
+// Topic 1.1: typeof Operator
+
+// Example 1: Checking primitive types
+console.log(typeof "Hello"); // Output: "string"
+console.log(typeof 42); // Output: "number"
+console.log(typeof true); // Output: "boolean"
