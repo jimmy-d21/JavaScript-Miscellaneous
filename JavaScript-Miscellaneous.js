871cda3a3e9afef1e3f0cf6976f4c2b0d19aaf29
jimmyedelacruz <jimmyedelacruz872@gmail.com>
2026-09-15 23:20:50 +0800

    The famous null bug

--- JavaScript-Miscellaneous.js
+++ JavaScript-Miscellaneous.js
@@ -1,5 +1,6 @@
 // 1. Type Checking
 // Topic 1.1: typeof Operator
+// typeof: Returns string representing primitive data type ("string", "number", "boolean", "undefined", "function", "object")
 
 // Example 1: Checking primitive types
 console.log(typeof "Hello"); // Output: "string"
@@ -10,3 +11,6 @@ console.log(typeof true); // Output: "boolean"
 let unassigned;
 console.log(typeof unassigned); // Output: "undefined"
 console.log(typeof function () {}); // Output: "function"
+
+// Example 3: The famous null bug
+console.log(typeof null); // Output: "object"
