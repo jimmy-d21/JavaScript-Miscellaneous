ef6a9b76a873b39fd4d0b9da0d50076de92ca49e
jimmyedelacruz <jimmyedelacruz872@gmail.com>
2026-09-15 23:20:15 +0800

    Checking undefined and functions

--- JavaScript-Miscellaneous.js
+++ JavaScript-Miscellaneous.js
@@ -5,3 +5,8 @@
 console.log(typeof "Hello"); // Output: "string"
 console.log(typeof 42); // Output: "number"
 console.log(typeof true); // Output: "boolean"
+
+// Example 2: Checking undefined and functions
+let unassigned;
+console.log(typeof unassigned); // Output: "undefined"
+console.log(typeof function () {}); // Output: "function"
