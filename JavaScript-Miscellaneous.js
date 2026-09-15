00ff18602ae429df38c2f3c8e3f57cf4501c23d4
jimmyedelacruz <jimmyedelacruz872@gmail.com>
2026-09-15 23:21:21 +0800

    Custom class instance check

--- JavaScript-Miscellaneous.js
+++ JavaScript-Miscellaneous.js
@@ -14,3 +14,10 @@ console.log(typeof function () {}); // Output: "function"
 
 // Example 3: The famous null bug
 console.log(typeof null); // Output: "object"
+
+// Topic 1.2: instanceof Operator
+// instanceof: Checks if an object is an instance of a specific class/constructor prototype
+// Example 1: Custom class instance check
+class User {}
+const user = new User();
+console.log(user instanceof User); // Output: true
